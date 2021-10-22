168df4853a6dbf52ca33cbf9b5db0c415ea4f6ef
Kchitiz21 <kchitiz.deep@knoldus.com>
2021-10-22 22:17:10 +0530

    Added code of error handling

---
+++ assignment_7/src/quest1/error_handle.rs
@@ -0,0 +1,15 @@
+pub fn enter_eveodd(num1: i32) -> Result<String, String> {
+    if num1 % 2 == 0 {
+        Ok("even".to_string())
+    } else {
+        Err("odd".to_string())
+    }
+}
+
+pub fn eveodd(num1: i32) -> String {
+    let answer = enter_eveodd(num1);
+    match answer {
+        Ok(answer) => answer,
+        Err(_v) => "Enter right integer".to_string(),
+    }
+}
