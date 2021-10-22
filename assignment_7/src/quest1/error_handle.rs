23bc6bbed9a64dd4d6cb63d0be255e8aad77eb48
Kchitiz21 <88480845+Kchitiz21@users.noreply.github.com>
2021-10-22 22:38:33 +0530

    Added code of error handling

--- assignment_7/src/quest1/error_handle.rs
+++ assignment_7/src/quest1/error_handle.rs
@@ -1,3 +1,12 @@
+/// enter_eveodd functions checks the number is even or odd
+///
+/// #Arguments
+///
+/// result : result control the number-value
+///
+/// #Return
+///
+/// return : return handle error and give output String.
 pub fn enter_eveodd(num1: i32) -> Result<String, String> {
     if num1 % 2 == 0 {
         Ok("even".to_string())
@@ -5,7 +14,15 @@ pub fn enter_eveodd(num1: i32) -> Result<String, String> {
         Err("odd".to_string())
     }
 }
-
+/// eveodd which handle the response of calling function
+///
+/// #Arguments
+///
+///num1: num1 is an Integer use to operate
+///
+/// #Return
+///
+/// Returns String which gives the function output
 pub fn eveodd(num1: i32) -> String {
     let answer = enter_eveodd(num1);
     match answer {
