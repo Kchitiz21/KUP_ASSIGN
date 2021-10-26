4677bc9b6bff106e884e810f32440c1e6fd5a66d
Kchitiz21 <88480845+Kchitiz21@users.noreply.github.com>
2021-10-26 09:36:09 +0530

    Added code of error handling

--- Assignments/assignment_7/src/test.rs
+++ Assignments/assignment_7/src/test.rs
@@ -7,8 +7,12 @@ mod tests {
     fn check_even() {
         assert_eq!(eveodd(25), "Enter right integer".to_string());
     }
+      #[test]
+    fn check_next() {
+        assert_eq!(eveodd(3), "Please put right integer".to_string());
+    }
     #[test]
     fn error_check() {
-        assert_ne!(eveodd(4), "Enter right integer".to_string());
+        assert_eq!(eveodd(4), "Enter right integer".to_string());
     }
 }
