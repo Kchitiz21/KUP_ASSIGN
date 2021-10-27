3d0be255a1007904cffa618b68c59cb300225624
Kchitiz21 <88480845+Kchitiz21@users.noreply.github.com>
2021-10-27 10:40:20 +0530

    Added code of generic module and custom iterator

--- Assignments/assignment_8/src/test.rs
+++ Assignments/assignment_8/src/test.rs
@@ -1,22 +1,21 @@
 #[cfg(test)]
-/// Compare Generic Numbers
+
 pub use crate::quest1::generic_min::minimum;
 #[test]
 pub fn generic() {
-    assert_eq!(minimum(10.7, 10.4),10.4);
+    assert_eq!(minimum(17.4, 14.4),14.4);
 }
 #[test]
 pub fn compare() {
-    assert_eq!(minimum(1, 100), 1);
+    assert_eq!(minimum(2,100), 2);
 }
 
-/// Sorting Generic Numbers
 pub use crate::quest1::generic_sort::sort;
 #[test]
 pub fn sorting_done() {
     assert_eq!(
-        sort(&mut vec![5, 1, 7, 8, 2, 9]),
-        vec![1, 2, 5, 7, 8, 9]
+        sort(&mut vec![7, 4, 1, 3, 2, 9]),
+        vec![9, 2, 5, 7, 4, 8]
     );
 }
 
@@ -26,24 +25,24 @@ pub  use crate::iterator::Iterator;
 #[test]
 pub fn gp_run() {
     let values = GeometricSeries {
-        first_number: 2,
-        ratio: 2,
-        length: 11,
+        first_number: 3,
+        ratio: 3,
+        length: 8,
     };
     assert_eq!(
         GeometricSeries::generate(values.first_number, values.ratio, values.length),
-        [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+        [3, 9, 27, 81, 243, 729, 2187, 6561]
     );
 }
 #[test]
 pub fn gp_check() {
     let values = GeometricSeries {
-        first_number: 1,
-        ratio: 2,
-        length: 11,
+        first_number: 4,
+        ratio: 4,
+        length: 8,
     };
     assert_eq!(
         GeometricSeries::generate(values.first_number, values.ratio, values.length),
-        [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+        [4, 16, 64,256, 1024, 4096, 16384, 65536]
     );
 }
