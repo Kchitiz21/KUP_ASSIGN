54e4d86ccf69d1b0632ba806adbc43e0030b3de9
Kchitiz21 <88480845+Kchitiz21@users.noreply.github.com>
2021-11-24 23:26:25 +0530

    Added code for asynchronous fashion

--- Assignments/assignment11/src/test.rs
+++ Assignments/assignment11/src/test.rs
@@ -5,12 +5,12 @@ mod tests {
     pub use crate::tasks::table_async::check;
 
     #[test]
-    fn tables_check_success() {
+    fn table_async_done() {
         assert_eq!(block_on(check()), ());
     }
 
     #[test]
-    fn synchronously_check() {
+    fn async_check() {
         assert_eq!(block_on(relate()), ());
     }
 }
