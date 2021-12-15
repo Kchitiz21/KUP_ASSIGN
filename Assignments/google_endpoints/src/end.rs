ada6ba40ec49c5b7b3daee21771c66f594e337fd
Kchitiz21 <88480845+Kchitiz21@users.noreply.github.com>
2021-12-15 21:53:35 +0530

    Added code for testing the endpoints of google

--- Assignments/google_endpoints/src/end.rs
+++ Assignments/google_endpoints/src/end.rs
@@ -17,12 +17,8 @@ pub async fn _testing() -> Result<(), reqwest::Error> {
         .get("https://www.googleapis.com/auth/calendar")
         .send()
         .await?;
-
     info!("Status: {}", res.status());
-
     let text = res.text().await?;
-
    info!("Body:{}", text);
-
     Ok(())
 }
