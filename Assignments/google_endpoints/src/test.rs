751e7574dbc685a1074d18f772a59b4643441ffa
Kchitiz21 <88480845+Kchitiz21@users.noreply.github.com>
2021-12-15 21:54:18 +0530

    Added code for testing the endpoints of google

--- Assignments/google_endpoints/src/test.rs
+++ Assignments/google_endpoints/src/test.rs
@@ -1,31 +1,23 @@
 #[cfg(test)]
 pub mod tests {
-
-
     #[tokio::test]
     pub async fn _reqwest() {
-
         let client = reqwest::Client::new();
-
         let res = client
             .get(&format!("https://www.googleapis.com/auth/calendar"))
             .send()
             .await
             .unwrap();
-
         assert_eq!(res.status(), reqwest::StatusCode::OK);
     }
 }
 #[tokio::test]
 pub async fn _reqwest() {
-
     let client = reqwest::Client::new();
-
     let res = client
         .get(&format!("https://www.googleapis.com/auth/calendar"))
         .send()
         .await
         .unwrap();
-
     assert_eq!(res.status(), reqwest::StatusCode::OK);
 }
